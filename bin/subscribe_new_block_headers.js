5fd0fa6b8de2025df43028c3986bdae51091c0a2
Peng Chi <pengchi@zjsk.onaliyun.com>
2018-04-04 15:37:13 +0800

    Add error handler

--- bin/subscribe_new_block_headers.js
+++ bin/subscribe_new_block_headers.js
@@ -11,6 +11,7 @@ const setTimeoutPromise = util.promisify(setTimeout);
 
 const LATEST_BLOCKNUMBER_KEY = 'coinmall_watched_latest_blocknumber';
 const WATCHED_TRANSACTION_KEY = 'coinmall_watched_transactions';
+const FAILED_TRANSACTION_KEY = 'coinmall_scan_failed_transaction_keys';
 
 redisClient.getAsync(LATEST_BLOCKNUMBER_KEY)
     .then(async (number) => {
@@ -21,7 +22,6 @@ redisClient.getAsync(LATEST_BLOCKNUMBER_KEY)
         while (true) {
             if (number < latestNumber) {
                 for (let i = number; i <= latestNumber; i++) {
-                    console.log(`Block number is ${i}`);
                     await scanTransactionsInBlock(i);
                     await redisClient.setAsync(LATEST_BLOCKNUMBER_KEY, i);
                 }
@@ -36,23 +36,38 @@ redisClient.getAsync(LATEST_BLOCKNUMBER_KEY)
 
 async function scanTransactionsInBlock(blockNumber) {
     const block = await web3.eth.getBlock(blockNumber);
-    console.log("Digging into " + block.hash.green.bold);
+    console.log("Digging into " + blockNumber + " " + block.hash.green.bold);
     await scanTransactions(block.transactions);
 }
 
 async function scanTransactions(transactions)
 {
-    let promisees = [];
-    for (let i = 0; i < transactions.length; i++) {
-        promisees.push(scanTransaction(transactions[i]));
+    const pageSize = 20;
+    const page = Math.ceil(transactions.length / pageSize);
+    for (let i = 0; i < page; i++) {
+        let promises = [];
+        let transactionSlice = transactions.slice(i*pageSize, (i+1)*pageSize);
+        transactionSlice.forEach(tx => {
+            promises.push(scanTransaction(tx));
+        });
+        try {
+            await Promise.all(promises)
+        } catch (e) {
+            console.error(e);
+        }
     }
-    return Promise.all(promisees);
 }
 
 async function scanTransaction(txHash)
 {
-    const tx = await web3.eth.getTransaction(txHash);
     console.log(`Scanning transaction ${txHash}...`);
+    try {
+        var tx = await web3.eth.getTransaction(txHash);
+    } catch (e) {
+        await redisClient.rpushAsync(FAILED_TRANSACTION_KEY, txHash);
+        return;
+    }
+
     if (null === tx || null === tx.to 
         || tx.to.toLowerCase() !== process.env.ATM_ADDRESS) {
         return;
